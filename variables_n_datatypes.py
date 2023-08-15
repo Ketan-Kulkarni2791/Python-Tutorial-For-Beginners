442bc079b6180724cfc7af4ac0cc86b49e87ccb6
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-08-15 23:40:06 +0530

    variables and datatypes added

---
+++ variables_n_datatypes.py
@@ -0,0 +1,21 @@
+# a = 100
+# print(a)    # o/p --> 100
+
+# # Chain of assignment
+# a = b = c = 100
+# print(a, b, c)      # o/p --> 100 100 100
+
+# # Same variable can be used for different datatype
+# var = 13.5
+# print(var)  # o/p --> 13.5
+# var = "Now I m a string"
+# print(var)  # o/p --> Now I m a string
+
+######################################### Datatypes #########################################
+
+# Integer
+
+print(123123123123123123123123123123123123123123123123 + 1)
+# o/p --> 123123123123123123123123123123123123123123123124
+
+

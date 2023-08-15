e82b7672bf2589203a498abd935669439e422d0c
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-08-15 20:45:20 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+print(45896)
+
+print(bool(0))
+
+print("Ketan", 'Kulkarni', sep='_')
+
+print("Ketan", end='__')
+print("Kulkarni")
+
+print(print("Hello World"))
+print(print(print(print(123))))
---
+++ comment_n_print.py
@@ -0,0 +1,22 @@
+# This is single line comment
+print("Hello World")            # o/p --> Hello World
+
+'''
+Following code line is the example of
+multi line comments
+'''
+print("Hello World")
+
+# Escape sequence character
+
+# \n -- new line
+print("Hello World \ Hello Ji World")   # o/p --> Hello World \ Hello Ji World
+
+print("Hello World \n Hello Ji World")  
+# o/p --> Hello World
+#          Hello Ji World
+
+# \' -- Single quote
+print("Hello 'Ji' World")   # o/p --> Hello 'Ji' World
+print('Hello \'Ji\' World') # o/p --> Hello 'Ji' World
+

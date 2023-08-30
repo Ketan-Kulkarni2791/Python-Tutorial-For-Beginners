93dbd2bfbcd9085231cb1d0f88aac1f44ad80299
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-08-30 23:22:03 +0530

    if else added

---
+++ if_else_statements.py
@@ -0,0 +1,24 @@
+i = 10
+
+if i < 15:
+    print("Hi There")       # o/p : Hi There
+    
+
+i = 20
+
+if i < 20:
+    print("less")
+else:
+    print("Greater")        # o/p : Greater
+
+
+i = 10
+
+if i < 15:
+    print("Less than 15")
+    if 1 < 12:
+        print("Less than 12 too")
+else:
+    print("Greater than 15")
+
+

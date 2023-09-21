2c058776c7a70fb49c35dc049333ee995ba4c5c1
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-09-21 17:27:30 +0530

    match cases and fol loop added

---
+++ for_loop.py
@@ -0,0 +1,31 @@
+n = 4
+for i in range(0, n):
+    print(i)
+
+
+# Iterating over a list
+print("List Iteration")
+l = ["Rachit", "Ketan", "Rahul"]
+for i in l:
+    print(i)
+    
+
+# Iterating over a tuple (immutable)
+print("\nTuple Iteration")
+t = ("Rachit", "Ketan", "Rahul")
+for i in t:
+    print(i)
+    
+
+# Iterating over a String
+print("\nString Iteration")
+s = "Hello World"
+for i in s:
+    print(i)
+    
+
+# Iterating over a set
+print("\nSet Iteration")
+set1 = {1, 2, 3, 4, 5, 6}
+for i in set1:
+    print(i)

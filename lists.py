058c37b3c277089c1bbed0a99213407b091132bc
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-10-05 18:48:34 +0530

    list notes and loop assignment added

--- lists.py
+++ lists.py
@@ -87,3 +87,72 @@ lst = list(map(int, input("Enter the integer elements:").strip().split()))[:n]
  
 # printing the list
 print('The list is:', lst) 
+
+
+# Python program to demonstrate Addition of elements in a List
+ 
+# Creating a List
+List = []
+print("Initial blank List: ")
+print(List)
+ 
+# Addition of Elements in the List
+List.append(1)
+List.append(2)
+List.append(4)
+print("\nList after Addition of Three elements: ")
+print(List)
+ 
+# Adding elements to the List using Iterator
+for i in range(1, 4):
+    List.append(i)
+print("\nList after Addition of elements from 1-3: ")
+print(List)
+ 
+# Adding Tuples to the List
+List.append((5, 6))
+print("\nList after Addition of a Tuple: ")
+print(List)
+ 
+# Addition of List to a List
+List2 = ['Bellamy', 'Blake']
+List.append(List2)
+print("\nList after Addition of a List: ")
+print(List)
+
+
+# Python program to demonstrate Addition of elements in a List
+  
+# Creating a List
+List = [1,2,3,4]
+print("Initial List: ")
+print(List)
+ 
+# Addition of Element at specific Position (using Insert Method)
+List.insert(3, 12)
+List.insert(0, 'Tokyo')
+print("\nList after performing Insert Operation: ")
+print(List)
+
+
+# Python program to demonstrate Addition of elements in a List
+ 
+# Creating a List
+List = [1, 2, 3, 4]
+print("Initial List: ")
+print(List)
+ 
+# Addition of multiple elements to the List at the end (using Extend Method)
+List.extend([8, 'Ratnakar', 'Matkari'])
+print("\nList after performing Extend Operation: ")
+print(List)
+
+
+# Reversing a list
+mylist = [1, 2, 3, 4, 5, 'Narayan', 'Dharap']
+mylist.reverse()
+print(mylist)
+
+my_list = [1, 2, 3, 4, 5]
+reversed_list = list(reversed(my_list))
+print(reversed_list)

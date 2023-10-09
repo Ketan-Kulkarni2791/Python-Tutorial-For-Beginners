2c26b119cc6bb3b73c4e2a3ee9bfe9dc35d01e26
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-10-10 04:38:31 +0530

    list comprehension added

---
+++ list_comprehension.py
@@ -0,0 +1,24 @@
+squares = []
+for i in range(10):
+    squares.append(i * i)
+print(squares)
+
+
+txns = [1.09, 23.56, 57.84, 4.56, 6.78]
+TAX_RATE = .08
+def get_price_with_tax(txn):
+    return txn * (1 + TAX_RATE)
+final_prices = map(get_price_with_tax, txns)
+print(list(final_prices))
+
+
+squares = [i * i for i in range(10)]
+print(squares)
+
+
+txns = [1.09, 23.56, 57.84, 4.56, 6.78]
+TAX_RATE = .08
+def get_price_with_tax(txn):
+    return txn * (1 + TAX_RATE)
+final_prices = [get_price_with_tax(i) for i in txns]
+print(final_prices)

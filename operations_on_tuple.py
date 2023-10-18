1ca937a12315035529d9e66a131ad91b5ee2f6f7
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-10-18 07:39:19 +0530

    operations on tuple added

--- operations_on_tuple.py
+++ operations_on_tuple.py
@@ -42,4 +42,41 @@ print("Elements between indices 0 and -4: ", tuple_[:-4])
 # Printing the entire tuple by using the default start and end values.     
 print("Entire tuple: ", tuple_[:])
 # o/p: Entire tuple:  ('Python', 'Tuple', 'Ordered', 'Immutable', 'Collection', 'Objects')
-  
+
+
+# Python program to show how to delete elements of a Python tuple    
+# Creating a tuple    
+tuple_ = ("Python", "Tuple", "Ordered", "Immutable", "Collection", "Objects")    
+# Deleting a particular element of the tuple    
+try:     
+    del tuple_[3]    
+    print(tuple_)    
+except Exception as e:    
+    print(e)            # o/p: 'tuple' object does not support item deletion  
+# Deleting the variable from the global space of the program    
+del tuple_    
+# Trying accessing the tuple after deleting it    
+try:    
+    print(tuple_)    
+except Exception as e:    
+    print(e)            # o/p: name 'tuple_' is not defined 
+    
+
+# Python program to show repetition in tuples    
+tuple_ = ('Python',"Tuples")    
+print("Original tuple is: ", tuple_)        # o/p: Original tuple is:  ('Python', 'Tuples')  
+# Repeting the tuple elements    
+tuple_ = tuple_ * 3    
+print("New tuple is: ", tuple_)   
+# o/p: New tuple is:  ('Python', 'Tuples', 'Python', 'Tuples', 'Python', 'Tuples')
+
+
+# Creating tuples  
+T1 = (0, 1, 5, 6, 7, 2, 2, 4, 2, 3, 2, 3, 1, 3, 2)  
+T2 = ('python', 'java', 'python', 'Tpoint', 'python', 'java')  
+# counting the appearance of 3  
+res = T1.count(2)  
+print('Count of 2 in T1 is:', res)      # o/p: Count of 2 in T1 is: 5 
+# counting the appearance of java  
+res = T2.count('java')  
+print('Count of Java in T2 is:', res)   # o/p: Count of java in T2 is: 2
